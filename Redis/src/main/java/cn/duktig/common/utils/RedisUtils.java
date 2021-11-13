0848bf31432ac2404a501f601b7d52a8e06f8c57
duktig666(RenShiWei) <1487660836@qq.com>
2021-11-13 11:16:02 +0800

    Redis 分布式锁

--- Redis/src/main/java/cn/duktig/common/utils/RedisUtils.java
+++ Redis/src/main/java/cn/duktig/common/utils/RedisUtils.java
@@ -11,6 +11,7 @@ import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.support.atomic.RedisAtomicLong;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
@@ -783,6 +784,56 @@ public class RedisUtils {
         RedisAtomicLong redisAtomicLong = new RedisAtomicLong(key, redisTemplate.getConnectionFactory());
         redisAtomicLong.expire(timeout, unit);
         return redisAtomicLong.addAndGet(increment);
+    }
+
+    // ---------------------Redis 分布式锁-----------------------
+
+    /**
+     * 加锁
+     *
+     * @param key   锁的唯一标志
+     * @param value 当前时间+超时时间  时间戳
+     * @return
+     */
+    public boolean lock(String key, String value) {
+        //对应setnx命令
+        if (redisTemplate.opsForValue().setIfAbsent(key, value)) {
+            //可以成功设置,也就是key不存在
+            return true;
+        }
+
+        //判断锁超时 - 防止原来的操作异常，没有运行解锁操作  防止死锁
+        String currentValue = (String) redisTemplate.opsForValue().get(key);
+        //如果锁过期
+        if (! StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue) < System.currentTimeMillis()) {
+            //currentValue不为空且小于当前时间
+            //获取上一个锁的时间value 对应getset，如果key存在
+            String oldValue = (String) redisTemplate.opsForValue().getAndSet(key, value);
+            //假设两个线程同时进来这里，因为key被占用了，而且锁过期了。获取的值currentValue=A(get取的旧的值肯定是一样的),两个线程的value都是B,key都是K.锁时间已经过期了。
+            //而这里面的getAndSet一次只会一个执行，也就是一个执行之后，上一个的value已经变成了B。只有一个线程获取的上一个值会是A，另一个线程拿到的值是B。
+            if (! StringUtils.isEmpty(oldValue) && oldValue.equals(currentValue)) {
+                //oldValue不为空且oldValue等于currentValue，也就是校验是不是上个时间戳，也是防止并发
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 解锁
+     *
+     * @param key
+     * @param value
+     */
+    public void unlock(String key, String value) {
+        try {
+            String currentValue = (String) redisTemplate.opsForValue().get(key);
+            if (! StringUtils.isEmpty(currentValue) && currentValue.equals(value)) {
+                redisTemplate.opsForValue().getOperations().delete(key);//删除key
+            }
+        } catch (Exception e) {
+            log.error("[Redis分布式锁] 解锁出现异常了，{}", e);
+        }
     }
 
 }
